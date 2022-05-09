af94fdb7a511bb7045940d24f1916ac9c64b86cc
Max Mindlin <maxmindlin@gmail.com>
2022-05-08 22:10:00 -0400

    placeholder

--- src/transitive_closure/mod.rs
+++ src/transitive_closure/mod.rs
@@ -1,2 +1,3 @@
 pub mod bfs;
 pub mod mtx;
+pub mod purdoms;
---
+++ src/transitive_closure/purdoms.rs
@@ -0,0 +1,24 @@
+use std::hash::Hash;
+
+use crate::mtx_graph::graph::Graph;
+
+use super::mtx::TransitiveClosureMtx;
+
+/// Calculates the transitive closure matrix
+/// of a given graph using purdoms algorithm.
+/// Time complexity:
+///   O(|E| + 𝜇|V|) where 𝜇 = # of strongly connected components.
+pub fn purdoms<T, D, W>(graph: &Graph<T, D, W>) -> TransitiveClosureMtx
+where
+    T: Hash + Eq + Clone,
+{
+    // 1. Find the strongly connected components of theoriginal graph,
+    //    replace each component by a single node, and remove the resulting loops.
+    // 2. Perform the topological sort of the acyclic graph 𝐺̃  obtained at stage 1.
+    // 3. Calculate the transitive closure of 𝐺̃ , moving from nodes with larger
+    //    indices to those with smaller ones.
+    // 4. Reconstruct the transitive closure of the original graph from the
+    //    transitive closure of 𝐺̃ .
+
+    unimplemented!()
+}
